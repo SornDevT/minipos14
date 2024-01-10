80a5434054e2732dcb68bbd117a665359e77cf6b
SornDevT <sorndev.teach@gmail.com>
2024-01-10 20:22:57 +0700

    first commit miniPos14

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Hello Vue App V111111</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos14App',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
