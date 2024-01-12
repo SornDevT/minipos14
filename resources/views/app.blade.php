23bb5f5b73271e1b7868abd127b60e23891c2ab5
SornDev <sorndev.teach@gmail.com>
2024-01-12 21:26:09 +0700

    install template HTML5 and Vue-Router VDO 03

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,18 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ url('assets/vendor/css/rtl/core.css') }}" class="template-customizer-core-css">
+    <link rel="stylesheet" href="{{ url('assets/vendor/css/rtl/theme-default.css') }}" class="template-customizer-theme-css">
+    <link rel="stylesheet" href="{{ url('assets/css/demo.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/font_lao.css') }}">
+    <!-- icon -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
     @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>Test Laravel Route V3</h1>
-    <p>jgiodfsjgiofjsiog fgdsjkgofpdsjkgiofd</p>
-    <h1>Update App V3</h1>
-    
-    <hr>
+
 
     <div id="app-vue"></div>
 
 
+    <script src="{{ url('assets/vendor/libs/jquery/jquery.js') }}"></script>
+  <script src="{{ url('assets/vendor/libs/popper/popper.js') }}"></script>
+  <script src="{{ url('assets/vendor/js/bootstrap.js') }}"></script>
+  <script src="{{ url('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+  <script src="{{ url('assets/vendor/libs/hammer/hammer.js') }}"></script>
+  <script src="{{ url('assets/vendor/libs/i18n/i18n') }}.js"></script>
+  <script src="{{ url('assets/vendor/libs/typeahead-js/typeahead.js') }}"></script>
+  <script src="{{ url('assets/vendor/js/menu.js') }}"></script>
+
+
     @vite('resources/js/app.js')
+
+
 </body>
 </html>
