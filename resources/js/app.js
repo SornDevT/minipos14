7dff852ebe9e08cf96c2d27892936368d8138e33
SornDev <sorndev.teach@gmail.com>
2024-01-15 21:04:14 +0700

    install vue-router and created table, controller, model and connect DataBase VDO 04

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,10 @@ import './bootstrap';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import MenuSidebar from './Components/MenuSidebar.vue'
 
 
 const app = createApp(App);
+app.component('MenuSidebar',MenuSidebar)
 app.use(router)
 app.mount('#app-vue');
